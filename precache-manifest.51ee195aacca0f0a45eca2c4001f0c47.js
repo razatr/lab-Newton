f6212bca1a44679d9f2088e0f340c14e10979984
Legolas <razatr@yandex.ru>
2020-10-15 17:01:58 +0300

    Updates

---
+++ precache-manifest.51ee195aacca0f0a45eca2c4001f0c47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87019801539ca725c11a852105fe02ea",
+    "url": "/lab-Newton/index.html"
+  },
+  {
+    "revision": "2fa0a2b1c973ee4665a5",
+    "url": "/lab-Newton/static/js/2.e4da2e7e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/lab-Newton/static/js/2.e4da2e7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3888a33ebe715c8497cd",
+    "url": "/lab-Newton/static/js/main.d4366aa8.chunk.js"
+  },
+  {
+    "revision": "5384bb1876e35e3064be",
+    "url": "/lab-Newton/static/js/runtime-main.b57e3d1f.js"
+  }
+]);
